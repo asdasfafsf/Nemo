1f1763b658a4400d123efb443ebbcc451e85ae08
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-03-23 14:51:11 +0900

    feat: code와 techMessage 추가

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,4 @@
-import { RESPONSE_CODE } from "../constants/common";
+import { RESPONSE_CODE, RESPONSE_MESSAGES, RESPONSE_TECH_MESSAGES } from "../constants/common";
 
 export type Request<T> = {
     key1: string;
@@ -9,9 +9,12 @@ export type Request<T> = {
 
 export type Response<T> = {
     code: ResponseCode;
-    message: string;
+    message: ResponseMessage;
+    techMessage: ResponseTechMessage;
     data: T;
 }
 
 
 export type ResponseCode = typeof RESPONSE_CODE[keyof typeof RESPONSE_CODE];
+export type ResponseMessage = typeof RESPONSE_MESSAGES[keyof typeof RESPONSE_MESSAGES];
+export type ResponseTechMessage = typeof RESPONSE_TECH_MESSAGES[keyof typeof RESPONSE_TECH_MESSAGES];
