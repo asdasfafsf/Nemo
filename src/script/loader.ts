20d3d536a0cc269832cb6ac61ff62cfebaaddb6e
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-03-27 16:59:50 +0900

    refactor: 다운로드와 read를 분리

--- src/script/loader.ts
+++ src/script/loader.ts
@@ -1,6 +1,5 @@
 import { readScript } from "./script";
 import { needDownload, downloadScript } from "./download";
-import { decryptScript } from "./decrypt";
 
 /**
  * 스크립트를 로드하는 함수
@@ -11,13 +10,12 @@ import { decryptScript } from "./decrypt";
  */
 export const loadScript = async ({key1, key2}: {key1: string, key2: string}): Promise<string> => {
     const need = await needDownload({key1, key2});
-    let script = '';
     
     if (need) {
-        script = await downloadScript({key1, key2});
-    } else {
-        script = await readScript({key1, key2});
-    }
+        await downloadScript({key1, key2});
+    } 
+
+    const script = await readScript({key1, key2});
     
-    return await decryptScript(script);
+    return script;
 }; 
