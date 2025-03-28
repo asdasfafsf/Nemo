ac5cd4966dbd3240fa70e586b561e3b3f9240685
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-03-28 21:16:34 +0900

    feat:

---
+++ src/script/executor.ts
@@ -0,0 +1,42 @@
+import { spawn } from "child_process";
+import { Response, Request } from "../types/common";
+import typia from "typia";
+import { RESPONSE_PAIR } from "../constants";
+
+/**
+ * 스크립트를 실행하는 함수
+ * @param param0 스크립트 키 정보
+ * @returns 스크립트 실행 결과
+ */
+export const executeScript = async (request: Request<any>): Promise<Response<any>> => {
+    if (!typia.is<Request<any>>(request)) {
+        return {
+            ...RESPONSE_PAIR.INVALID_PARAMETERS,
+            data: null
+        }
+    }
+
+    const finalConfig = {
+        timeout: 30000,
+        memory: 300,
+        ...request.config
+    };
+    const config = finalConfig;
+    const { key1, key2 } = request;
+
+    const script = spawn('node', [key1, key2]);
+
+
+    return new Promise((resolve, reject) => {
+        script.stdout.on('data', (data) => {
+            if (typia.is<Response<any>>(data)) {
+                resolve(data);
+            } else {
+                
+            }
+        });
+        script.stderr.on('data', (data) => {
+            console.error(data.toString());
+        });
+    });
+};
