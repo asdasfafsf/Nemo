8e64341c702e73ef1e302a2d6ae35fb1a1caa156
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-03-28 20:52:00 +0900

    feat: 결과 메세지 타입 string 수정

--- src/types/common.ts
+++ src/types/common.ts
@@ -21,5 +21,5 @@ export type ResponseCode =
     | (typia.tags.Pattern<"^[1][0-9]{4}$"> & 
     typia.tags.MinLength<5> & 
     typia.tags.MaxLength<5>);
-export type ResponseMessage = typeof RESPONSE_MESSAGES[keyof typeof RESPONSE_MESSAGES];
-export type ResponseTechMessage = typeof RESPONSE_TECH_MESSAGES[keyof typeof RESPONSE_TECH_MESSAGES];
+export type ResponseMessage = string;
+export type ResponseTechMessage = string;
