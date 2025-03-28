a68e8e45c8a76d7d7515d67a50df76b8670b4986
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-03-28 20:47:49 +0900

    feat: 오류코드 추가

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,5 +1,5 @@
 import { RESPONSE_CODE, RESPONSE_MESSAGES, RESPONSE_TECH_MESSAGES } from "../constants/common";
-
+import typia from "typia";
 export type Request<T> = {
     key1: string;
     key2: string;
@@ -7,14 +7,19 @@ export type Request<T> = {
 }
 
 
+
 export type Response<T> = {
-    code: ResponseCode;
+    code: ResponseCode 
     message: ResponseMessage;
     techMessage: ResponseTechMessage;
     data: T;
 }
 
 
-export type ResponseCode = typeof RESPONSE_CODE[keyof typeof RESPONSE_CODE];
+export type ResponseCode = 
+    typeof RESPONSE_CODE[keyof typeof RESPONSE_CODE]
+    | (typia.tags.Pattern<"^[1][0-9]{4}$"> & 
+    typia.tags.MinLength<5> & 
+    typia.tags.MaxLength<5>);
 export type ResponseMessage = typeof RESPONSE_MESSAGES[keyof typeof RESPONSE_MESSAGES];
 export type ResponseTechMessage = typeof RESPONSE_TECH_MESSAGES[keyof typeof RESPONSE_TECH_MESSAGES];
