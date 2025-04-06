43422ae849f2099dfc411f4112787fb8ee886648
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-04-06 14:44:41 +0900

    feat: 최상위 스코프를 async로 감쌈

--- src/script/worker.ts
+++ src/script/worker.ts
@@ -1,37 +1,42 @@
 import path from 'path';
 import { RESPONSE_PAIR } from '../constants/common';
 import env from '../config/env';
+import typia from 'typia';
 
 
 const scriptRootPath = env.scriptRootPath || './scripts';
 const [nodePath, workerPath, key1, key2, dataJson] = process.argv;
+(async () => {
+    try {
+        const scriptPath = path.resolve(scriptRootPath, `${key1}.js`);
+        const script = await import(scriptPath);
 
-console.log('워커를 실행해요!11111')
-try {
-    const scriptPath = path.resolve(scriptRootPath, `${key1}.js`);
-    
-    // require() 대신 동적 import() 사용
-    const script = await import(scriptPath);
+        if (typeof script.nemo !== 'function') {
+            console.log(JSON.stringify({
+                ...RESPONSE_PAIR.SCRAPING_ENGINE_UNDEFINED_NEMO,
+                data: null
+            }));
+            process.exit(0);
+        }
+        
+        const data = dataJson ? JSON.parse(dataJson) : {};
 
-    console.log('워커를 실행해요!')
-    
-    if (typeof script.nemo !== 'function') {
+        const result = await script.nemo(data);
+        if (!typia.is<Response>(result)) {
+            console.log(JSON.stringify({
+                ...RESPONSE_PAIR.INVALID_OUTPUT_FORMAT,
+                data: result
+            }));
+            process.exit(0);
+        }
+
+        console.log(JSON.stringify(result));
+        process.exit(0);
+    } catch (error) {
         console.log(JSON.stringify({
-            ...RESPONSE_PAIR.SCRAPING_ENGINE_UNDEFINED_NEMO,
+            ...RESPONSE_PAIR.ERROR,
             data: null
         }));
-        process.exit(0);
+        process.exit(1);
     }
-    
-    const data = dataJson ? JSON.parse(dataJson) : {};
-    
-    const result = script.nemo(data);
-    console.log(JSON.stringify(result));
-    process.exit(0);
-} catch (error: any) {
-    console.log(JSON.stringify({
-        ...RESPONSE_PAIR.ERROR,
-        data: null
-    }));
-    process.exit(1);
-}
+})();
