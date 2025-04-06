5d0f5203a29229073140a8d3da1efbee42766b7d
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-04-06 20:39:41 +0900

    feat:

--- src/script/worker.ts
+++ src/script/worker.ts
@@ -1,14 +1,11 @@
-import path from 'path';
 import { RESPONSE_PAIR } from '../constants/common';
-import env from '../config/env';
 import typia from 'typia';
+import { Response } from '../types';
 
 
-const scriptRootPath = env.scriptRootPath || './scripts';
-const [nodePath, workerPath, key1, key2, dataJson] = process.argv;
-(async () => {
+const [nodePath, scriptPath, key2, dataJson] = process.argv;
+(async function() {
     try {
-        const scriptPath = path.resolve(scriptRootPath, `${key1}.js`);
         const script = await import(scriptPath);
 
         if (typeof script.nemo !== 'function') {
@@ -16,27 +13,23 @@ const [nodePath, workerPath, key1, key2, dataJson] = process.argv;
                 ...RESPONSE_PAIR.SCRAPING_ENGINE_UNDEFINED_NEMO,
                 data: null
             }));
-            process.exit(0);
         }
         
         const data = dataJson ? JSON.parse(dataJson) : {};
 
         const result = await script.nemo(data);
-        if (!typia.is<Response>(result)) {
+        if (!typia.is<Response<any>>(result)) {
             console.log(JSON.stringify({
                 ...RESPONSE_PAIR.INVALID_OUTPUT_FORMAT,
                 data: result
             }));
-            process.exit(0);
         }
 
         console.log(JSON.stringify(result));
-        process.exit(0);
     } catch (error) {
         console.log(JSON.stringify({
             ...RESPONSE_PAIR.ERROR,
             data: null
         }));
-        process.exit(1);
     }
-})();
+}());
