93e3e1314827a031e9c64ca06bcde5d2a2489431
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-04-06 20:40:20 +0900

    log: 로그 추가

--- src/script/loader.ts
+++ src/script/loader.ts
@@ -1,5 +1,6 @@
 import { needDownload, downloadScript } from "./download";
-
+import { executeScript } from "./executor";
+import { Request } from "../types/common";
 /**
  * 스크립트를 로드하는 함수
  * @param {Object} param0 - 파라미터 객체
@@ -8,9 +9,17 @@ import { needDownload, downloadScript } from "./download";
  * @returns {Promise<void>}
  */
 export const loadScript = async ({key1, key2}: {key1: string, key2: string}) => {
+    console.log('다운로드 필요 여부 확인 시작')
     const need = await needDownload({key1, key2});
-    
+    console.log('다운로드 필요 여부 확인 완료')
     if (need) {
+        console.log('다운로드 시작')
         await downloadScript({key1, key2});
+        console.log('다운로드 완료')
     } 
 };
+
+export const runScript = async (request: Request<unknown>) => {
+    console.log('스크립트 실행 시작')
+    return await executeScript(request);
+}
