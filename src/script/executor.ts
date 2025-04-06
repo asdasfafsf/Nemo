21dbe07f0d5af7b58d6589848a4f0a8a4fc4755f
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-04-06 20:40:14 +0900

    feat: 타임아웃 추가

--- src/script/executor.ts
+++ src/script/executor.ts
@@ -2,14 +2,15 @@ import { spawn } from "child_process";
 import { Response, Request } from "../types/common";
 import typia from "typia";
 import { RESPONSE_PAIR } from "../constants";
-
+import path from "path";
+import env from "../config/env";
 /**
  * 스크립트를 실행하는 함수
  * @param param0 스크립트 키 정보
  * @returns 스크립트 실행 결과
  */
-export const executeScript = async (request: Request<any>): Promise<Response<any>> => {
-    if (!typia.is<Request<any>>(request)) {
+export const executeScript = async (request: Request<unknown>): Promise<Response<any>> => {
+    if (!typia.is<Request<unknown>>(request)) {
         return {
             ...RESPONSE_PAIR.INVALID_PARAMETERS,
             data: null
@@ -17,21 +18,37 @@ export const executeScript = async (request: Request<any>): Promise<Response<any
     }
 
     const finalConfig = {
-        timeout: 30000,
+        timeout: 0,
         memory: 300,
         ...request.config
     };
     const config = finalConfig;
     const { key1, key2 } = request;
 
-    const script = spawn('node', [key1, key2]);
-
+    const script = spawn(
+        'node', 
+        [
+            path.resolve(env.scriptRootPath, `${key1}.js`),
+            key2,
+            JSON.stringify(request.data)
+        ]
+    );
 
     return new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => {
+            resolve({
+                ...RESPONSE_PAIR.REQUEST_TIMEOUT,
+                data: null
+            });
+            script.kill();
+        }, Number(config.timeout));
+
         script.stdout.on('data', (data) => {
             if (typia.is<Response<any>>(data)) {
+                clearTimeout(timeout);  
                 resolve(data);
             } else {
+                clearTimeout(timeout);  
                 resolve({
                     ...RESPONSE_PAIR.INVALID_OUTPUT_FORMAT,
                     data: null
@@ -39,7 +56,19 @@ export const executeScript = async (request: Request<any>): Promise<Response<any
             }
         });
         script.stderr.on('data', (data) => {
-            console.error(data.toString());
+            clearTimeout(timeout);  
+            resolve({
+                ...RESPONSE_PAIR.ERROR,
+                data: null
+            });
+        });
+
+        script.on('close', (code) => {
+            clearTimeout(timeout);  
+            resolve({
+                ...RESPONSE_PAIR.ERROR,
+                data: null
+            });
         });
     });
 };
