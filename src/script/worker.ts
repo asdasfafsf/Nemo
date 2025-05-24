a34693f8224963242333b08baa4f38aed30e58f9
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-24 15:16:57 +0900

    fix: node_modules를 못찾는 오류 수정

--- src/script/worker.ts
+++ src/script/worker.ts
@@ -2,11 +2,28 @@ import { RESPONSE_PAIR } from '../constants/common';
 import typia from 'typia';
 import { Response } from '../types';
 import process from 'process';
+import { pathToFileURL } from 'url';
+import Module from 'module';
+import path from 'path';
+import fs from 'fs';
 
 const [nodePath, workerPath,scriptPath, key2, dataJson] = process.argv;
+
+async function loadScript(file: string) {
+    const dir = path.dirname(file);
+    const realNm = path.resolve(process.cwd(), 'node_modules');
+    const linkNm = path.join(dir, 'node_modules');
+  
+    if (!fs.existsSync(linkNm)) {
+      fs.symlinkSync(realNm, linkNm, 'dir');
+    }
+  
+    return await import(pathToFileURL(file).href);
+}
+
 (async function() {
     try {
-        const script = await import(scriptPath);
+        const script = await loadScript(scriptPath);
         if (typeof script.nemo !== 'function') {
             process.stdout.write(JSON.stringify({
                 ...RESPONSE_PAIR.SCRAPING_ENGINE_UNDEFINED_NEMO,
@@ -15,7 +32,7 @@ const [nodePath, workerPath,scriptPath, key2, dataJson] = process.argv;
 
             return;
         }
-        
+
         const data = dataJson ? JSON.parse(dataJson) : {};
 
         const result = await script.nemo(data);
